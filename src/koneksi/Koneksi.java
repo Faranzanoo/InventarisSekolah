bc1094f2234553c7493eb1a67b9817bbefcb4298
Titan Faranzano <titanfaranzano214@gmail.com>
2026-06-26 21:18:01 +0700

    Initial commit: Aplikasi Inventaris Sekolah

---
+++ src/koneksi/Koneksi.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package koneksi;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Koneksi {
+
+    private static Connection koneksi;
+
+    public static Connection getKoneksi() {
+        if (koneksi == null) {
+            try {
+                String url = "jdbc:mysql://localhost:3306/db_inventaris_sekolah";
+                String user = "root";
+                String password = "";
+
+                DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
+                koneksi = DriverManager.getConnection(url, user, password);
+
+                System.out.println("Koneksi Berhasil!");
+            } catch (SQLException e) {
+                System.out.println("Koneksi Gagal : " + e.getMessage());
+            }
+        }
+        return koneksi;
+    }
+}
